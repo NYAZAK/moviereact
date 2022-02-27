b541f57bdfe200bb4e904e2489d5e69c1168ca44
ZAKARIA <nyzakaria@icloud.com>
2022-02-27 18:08:04 +0100

    add the base of project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Header, MovieDetails, MovieList } from "./components";
 
-function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<div className="d-flex flex-row" >
+					<MovieList />
+					<MovieDetails />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
